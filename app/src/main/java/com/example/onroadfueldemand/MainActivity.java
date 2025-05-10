9226057363874e01585468c58ff757acae57d3dd
Mohamma734 <126301493+Mohamma734@users.noreply.github.com>
2025-05-11 00:22:12 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/onroadfueldemand/MainActivity.java
+++ app/src/main/java/com/example/onroadfueldemand/MainActivity.java
@@ -2,56 +2,53 @@ package com.example.onroadfueldemand;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity{
-
-    //Object Declaration
-    Button user,admin,bunk;
+    Button user, admin, bunk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //TypeCasting
-        user= findViewById(R.id.user);
+        user = findViewById(R.id.user);
         admin = findViewById(R.id.admin);
         bunk = findViewById(R.id.bunk);
 
-        //code for users actions
-        user.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, Login.class);
-                i.putExtra("key", "user");
-                startActivity(i);
-            }
-        });
+        user.setOnClickListener(v -> openLogin("user"));
+        admin.setOnClickListener(v -> openLogin("admin"));
+        bunk.setOnClickListener(v -> openLogin("bunk"));
+    }
 
-        //code for admin actions
-        admin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i =new Intent(MainActivity.this, Login.class);
-                i.putExtra("key", "admin");
-                startActivity(i);
-            }
-        });
+    private void openLogin(String userType) {
+        Intent intent = new Intent(this, Login.class);
+        intent.putExtra("key", userType);
+        startActivity(intent);
+    }
 
-        //code for bunk actions
-        bunk.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    // إظهار القائمة (الثلاث نقاط)
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
-                Intent i =new Intent(MainActivity.this, Login.class);
-                i.putExtra("key", "bunk");
-                startActivity(i);
-            }
-        });
+    // التعامل مع النقر على عنصر القائمة
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_about_company) {
+            Intent intent = new Intent(this, AboutCompanyActivity.class);
+            startActivity(intent);
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
